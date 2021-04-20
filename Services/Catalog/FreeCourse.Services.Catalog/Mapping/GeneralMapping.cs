4800a26c3aca3eb84e2231e0b03236bb739f2dad
Emrah Atalay <emrahatalay92@gmail.com>
2021-04-20 21:44:57 +0300

    Add project files.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Mapping/GeneralMapping.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Course, CourseDto>();
+            CreateMap<Category, CategoryDto>();
+            CreateMap<CategoryDto, Category>();
+            CreateMap<Feature, FeatureDto>();
+
+            CreateMap<Course, CourseCreateDto>();
+            CreateMap<Course, CourseUpdateDto>();
+        }
+    }
+}
